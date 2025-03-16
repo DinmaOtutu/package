4b2d79dc157b371d9c12154bdb26ef09646c6738
DinmaOtutu <otutudinma1995@gmail.com>
2025-03-16 23:10:36 +0100

    remove console

--- client/index.js
+++ client/index.js
@@ -14,12 +14,10 @@ const Index = () => {
   const [showingSTIFModal, setShowingSTIFModal] = useState(false);
 
   const hostUserId = 'host_user_1';
-console.log('hostUserId', hostUserId);
   useEffect(() => {
     setCalendarResponse(null);
 
     let url = `/api/calendar?hostUserId=${hostUserId}`;
-    console.log('url', url);
     if (guestUserId) url += `&guestUserId=${guestUserId}`;
     fetch(url)
       .then((response) => response.json())
@@ -29,7 +27,6 @@ console.log('hostUserId', hostUserId);
   }, [guestUserId]);
 
   if (!calendarResponse) {
-    console.log('Loading...');
     return <div>Loading...</div>;
   }
 
